f05de359ee93c3644f1a1d2a4cf05d043f5a1d23
sinhye-design <sinhye3080@gmail.com>
2024-12-04 15:58:32 +0900

    연동테스트01

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<!-- 연동 테스트 -->
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import
+
 
 // https://vite.dev/config/
 export default defineConfig({
